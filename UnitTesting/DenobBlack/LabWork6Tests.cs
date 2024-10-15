d23a337414948118563144532978ef1dd3af90fe
DenobBlack <egor19-1205.baranov@yandex.ru>
2024-10-15 11:26:57 +0300

    Add LabWork6 and LabWork7

---
+++ UnitTesting/DenobBlack/LabWork6Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Math;
+
+namespace UnitTesting.DenobBlack
+{
+    public class LabWork6Tests
+    {
+        private readonly BasicCalc _calculator;
+
+        public LabWork6Tests()
+        {
+            _calculator = new BasicCalc();
+        }
+
+        [Fact]
+        public void Sqrt_ShouldReturnCorrectSqrt()
+        {
+            double result = _calculator.Sqrt(16);
+            Assert.Equal(4, result);
+        }
+
+        [Theory]
+        [InlineData(4, 2)]
+        [InlineData(16, 4)]
+        [InlineData(7, 2.6)]
+        public void Sqrt_Theory(double number, double expectedResult)
+        {
+            double result = _calculator.Sqrt(number);
+            Assert.Equal(expectedResult, result, 0.1);
+        }
+
+        [Fact]
+        public void Sqrt_ShouldThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Sqrt(-1));
+        }
+
+        [Fact]
+        public void IsPerfectNumber_ShouldReturnCorrectResult()
+        {
+            bool result = _calculator.IsPerfectNumber(6);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(6, true)]
+        [InlineData(28, true)]
+        [InlineData(7, false)]
+        public void IsPerfectNumber_Theory(int number, bool expectedResult)
+        {
+            bool result = _calculator.IsPerfectNumber(number);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void IsPerfectNumber_ShouldThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.IsPerfectNumber(-1));
+        }
+    }
+}
---
+++ UnitTesting/DenobBlack/LabWork7Tests.cs
@@ -0,0 +1,101 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Weather;
+using TestingLib.Shop;
+using TestingLib.Library;
+
+namespace UnitTesting.DenobBlack
+{
+    public class LabWork7Tests
+    {
+        private readonly Mock<IWeatherForecastSource> mockWeatherForecastSource;
+        private readonly Mock<ICustomerRepository> mockCustomerRepository;
+        private readonly Mock<INotificationService> mockNotificationService;
+        private readonly Mock<IOrderRepository> mockOrderRepository;
+
+        public LabWork7Tests()
+        {
+            mockWeatherForecastSource = new Mock<IWeatherForecastSource>();
+            mockCustomerRepository = new Mock<ICustomerRepository>();
+            mockNotificationService = new Mock<INotificationService>();
+            mockOrderRepository = new Mock<IOrderRepository>();
+        }
+
+        [Fact]
+        public void GetWeatherForecast_ShouldReturnCorrectResult()
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecast { TemperatureC = 27, Summary = "Солнечно" };
+            var currentDate = DateTime.Now; 
+
+            mockWeatherForecastSource.Setup(repo => repo.GetForecast(currentDate)).Returns(weatherForecast);
+
+            var service = new WeatherForecastService(mockWeatherForecastSource.Object);
+
+            // Act
+            var result = service.GetWeatherForecast(currentDate);
+
+            // Assert
+            Assert.NotNull(result);
+            mockWeatherForecastSource.Verify(repo => repo.GetForecast(It.IsAny<DateTime>()), Times.Once);
+        }
+
+        [Fact]
+        public void CreateOrder_ShouldCreateNewOrder()
+        {
+            // Arrange
+            var customer = new Customer { Id = 1, Name = "Максим", Email = "max@gmail.com" };
+            var order = new Order { Id = 10, Date = DateTime.Now, Customer = customer, Amount = 10 };
+            mockOrderRepository.Setup(repo => repo.GetOrderById(1)).Returns(order);
+
+            var service = new ShopService(mockCustomerRepository.Object, mockOrderRepository.Object, mockNotificationService.Object);
+
+            // Act
+            service.CreateOrder(order);
+
+            // Assert
+            mockOrderRepository.Verify(repo => repo.GetOrderById(It.IsAny<int>()), Times.Once);
+            mockOrderRepository.Verify(repo => repo.AddOrder(It.IsAny<Order>()), Times.Once);
+        }
+
+        [Fact]
+        public void CreateOrder_ShouldSendNotification()
+        {
+            // Arrange
+            var customer = new Customer { Id = 1, Name = "Максим", Email = "max@gmail.com" };
+            var order = new Order { Id = 10, Date = DateTime.Now, Customer = customer, Amount = 10 };
+            mockOrderRepository.Setup(repo => repo.GetOrderById(1)).Returns(order);
+
+            var service = new ShopService(mockCustomerRepository.Object, mockOrderRepository.Object, mockNotificationService.Object);
+
+            // Act
+            service.CreateOrder(order);
+
+            // Assert
+            mockNotificationService.Verify(repo => repo.SendNotification(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetCustomerInfo_ShouldReturnCorrectResult()
+        {
+            // Arrange
+            var customer = new Customer { Id = 1, Name = "Максим", Email = "max@gmail.com" };
+            var order = new Order { Id = 1, Date = DateTime.Now, Customer = customer, Amount = 10 };
+            mockOrderRepository.Setup(repo => repo.GetOrders()).Returns(new List<Order> { order });
+            mockCustomerRepository.Setup(repo => repo.GetCustomerById(1)).Returns(customer);
+
+            var service = new ShopService(mockCustomerRepository.Object, mockOrderRepository.Object, mockNotificationService.Object);
+            // Act
+            string result = service.GetCustomerInfo(1);
+
+            // Assert
+            Assert.Equal("Customer Максим has 1 orders", result);
+            mockCustomerRepository.Verify(repo => repo.GetCustomerById(It.IsAny<int>()), Times.Once);
+            mockOrderRepository.Verify(repo => repo.GetOrders(), Times.Once);
+        }
+    }
+}
